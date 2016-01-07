33bd7dfeea56e0fbc8b1cb34bad8b5cf988595f7
melhzy <zhuang@sqlgeek.org>
2016-01-06 19:34:20 -0500

    new smart reporting;

---
+++ SmartReporting_HW2/SmartReporting_HW2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmartReporting_HW2.MvcApplication" Language="C#" %>
---
+++ SmartReporting_HW2/SmartReporting_HW2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartReporting_HW2.Startup))]
+namespace SmartReporting_HW2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
